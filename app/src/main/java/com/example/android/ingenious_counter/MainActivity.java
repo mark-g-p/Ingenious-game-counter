bfb83c237d1d5b5892cd565ff7f9ff091ba13511
Marek <marek.g.przybylowicz@gmail.com>
2018-01-05 17:50:35 +0100

    Dump whole java code at once, because you forgot to commit it earlier...

--- app/src/main/java/com/example/android/ingenious_counter/MainActivity.java
+++ app/src/main/java/com/example/android/ingenious_counter/MainActivity.java
@@ -1,21 +1,22 @@
 package com.example.android.ingenious_counter;
 
+import android.graphics.drawable.ColorDrawable;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TableRow;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 
 public class MainActivity extends AppCompatActivity {
 
 
-    private Player player1;
-    private Player player2;
-    private Player player3;
-    private Player player4;
     private ArrayList<Player> players;
     private int active_player;
 
@@ -26,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         start();
     }
 
-    private class Player {
+    protected class Player implements Comparable<Player> {
 
         private String name;
         private ArrayList<Integer> points;
@@ -35,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         /**
          * @param pointsTable row from table of points, contains player's name and points.
          */
-        public Player(TableRow pointsTable) {
+        private Player(TableRow pointsTable) {
 
             final int index = pointsTable.getChildCount();
             tableRow = pointsTable;
@@ -52,24 +53,35 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        public TableRow getTableRow() {
+        @Override
+        public int compareTo(@NonNull Player otherPlayer) {
+            return this.getScore() - otherPlayer.getScore();
+        }
+
+        TableRow getTableRow() {
             return tableRow;
         }
 
-        public String getName() {
+        String getName() {
             return name;
         }
 
-        public int getScore() {
+        int getScore() {
             return points.get(0);
         }
 
-        public int getIthPoints(int index) {
+        int getIthPoints(int index) {
             return points.get(index);
         }
 
-        public void addPointTo(int index) {
-
+        void addPointTo(int index) {
+            int currentPoints = points.get(index);
+            if (currentPoints < 18) {
+                points.set(index, ++currentPoints);
+                //Set first element of the points as player's score.
+                points.set(0, Collections.min(points.subList(1, points.size())));
+                displayPoints(players.get(active_player));
+            }
         }
     }
 
@@ -78,31 +90,65 @@ public class MainActivity extends AppCompatActivity {
      */
     public void start() {
 
+        players = new ArrayList<>(0);
+
+        TableRow rowTable = findViewById(R.id.player1);
+        Player player1 = new Player(rowTable);
+        displayPoints(player1);
+        players.add(player1);
+
+        rowTable = findViewById(R.id.player2);
+        Player player2 = new Player(rowTable);
+        displayPoints(player2);
+        players.add(player2);
+
+        rowTable = findViewById(R.id.player3);
+        Player player3 = new Player(rowTable);
+        displayPoints(player3);
+        players.add(player3);
+
+        rowTable = findViewById(R.id.player4);
+        Player player4 = new Player(rowTable);
+        displayPoints(player4);
+        players.add(player4);
+
+        revertColors(players.get(active_player).getTableRow().getChildAt(0));
+        active_player = 0;
+        setActivePlayer(players.get(active_player).getTableRow().getChildAt(0));
+        displayWinner("None");
     }
 
     /*
     * Fuctions used by buttons*/
     public void addPoint1(View view) {
-
+        players.get(active_player).addPointTo(1);
     }
 
     public void addPoint2(View view) {
+        players.get(active_player).addPointTo(2);
     }
 
     public void addPoint3(View view) {
+        players.get(active_player).addPointTo(3);
     }
 
     public void addPoint4(View view) {
+        players.get(active_player).addPointTo(4);
     }
 
     public void addPoint5(View view) {
+        players.get(active_player).addPointTo(5);
     }
 
     public void addPoint6(View view) {
+        players.get(active_player).addPointTo(6);
     }
 
     //    Give turn to the next player. Back to first player after one round.
     public void nextPlayer(View view) {
+        revertColors(players.get(active_player).getTableRow().getChildAt(0));
+        active_player = (active_player + 1) % 4;
+        setActivePlayer(players.get(active_player).getTableRow().getChildAt(0));
     }
 
     // Resets all points 0.
@@ -111,10 +157,48 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void getWinner(View view) {
+        String winner = Collections.max(players).getName();
+        displayWinner(winner);
     }
 
     // Displaying functions.
     private void displayWinner(String winner) {
+        TextView currentlyWinning = findViewById(R.id.winner);
+        currentlyWinning.setText(winner);
+    }
+
+    //
+    private void revertColors(View view) {
+        view.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPointsTable));
+    }
+
+    private void setActivePlayer(View view) {
+        view.setBackgroundColor(ContextCompat.getColor(this, R.color.colorActive));
+    }
+
+    private void displayPoints(Player playersPoints) {
+
+        TableRow playersPointsView = playersPoints.getTableRow();
+
+        int index = playersPointsView.getChildCount();
+
+        for (int i = 1; i < index; i++) {
+            TextView pointCell = (TextView) playersPointsView.getChildAt(i);
+            int point = playersPoints.getIthPoints(i);
+
+//             Keep original color of TextView by giving it transparent background
+            revertColors(pointCell);
+
+//             When player reaches 18 points in one color he gets 'genius'.
+//             Change color of TextView when it happens.
+            if (point == 18) {
+                TableRow buttonPanel = findViewById(R.id.button_panel);
+                Button button = (Button) buttonPanel.getChildAt(i);
+                ColorDrawable buttonColor = (ColorDrawable) button.getBackground();
+                pointCell.setBackgroundColor(buttonColor.getColor());
+            }
+            pointCell.setText(String.valueOf(point));
+        }
     }
 
 
